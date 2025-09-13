7e70c7621f9c6f79ae7fe2a4132558d4d4ed6331
Murugamoorthy Dinushan <dinushandinumoorthy1111@gmail.com>
2025-09-13 21:21:41 +0530

    Add files via upload

---
+++ 03.getting_input.py
@@ -0,0 +1,24 @@
+"""#Getting string input statement
+name = input("Enter Name : ") #if given {25} or {name}, it will shown as string
+print(name)
+print(type(name))"""
+
+"""#Getting Integer input statement
+a = int(input("Enter The Value of A : "))
+b = int(input("Enter The Value of B : "))
+c = a+b
+print(c)
+print(type(c))"""
+
+#Getting Float input statement
+a = float(input("Enter The Value of A : "))
+b = float(input("Enter The Value of B : "))
+c = a+b
+print(c)
+print(type(c))
+
+###NOTE: Default aa input() => get inputs and store by considering all strings
+### {double ""} whatever we give "" anything, it will shown the message.
+
+
+
