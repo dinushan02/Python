5e6f4007c2b83637157e6a4ac26998eed99b3ee4
Murugamoorthy Dinushan <dinushandinumoorthy1111@gmail.com>
2025-09-13 21:28:01 +0530

    Add files via upload

---
+++ 21.While & For Else.py
@@ -0,0 +1,16 @@
+"""i = 1
+while i <= 5:
+    #if i == 4:
+     #   break
+    print(i)
+    i += 1
+else:
+    print("Loop completed!") # This will execute if the loop wasn't terminated by a break statement"""
+
+for i in range(1, 21):
+    #if i == 5:
+     #   break
+    print(i)
+else:
+    print("For loop completed!")
+
