07ed11417f74f9d2f99d3399d0c02ab77754dcf4
Murugamoorthy Dinushan <dinushandinumoorthy1111@gmail.com>
2025-09-13 21:21:25 +0530

    Add files via upload

---
+++ 02.keywords.py
@@ -0,0 +1,6 @@
+#keywords =are Reserved words in Python
+#We cannot use a keyword as a variable name,function name or other identifier.
+#Keywords are case sensitive in Python
+import keyword #package keyword in import
+print(keyword.kwlist)
+#this two lines show all keywords in Python
